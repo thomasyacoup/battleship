1c5d17309567d3a4fb9a773cc95bde10368cec15
thomas011001 <thomasyacoub321@gmail.com>
2026-08-17 07:56:07 +0300

    FEAT: implement Ship class and subclasses for different ship types

---
+++ src/js/modules/ship.js
@@ -0,0 +1,37 @@
+class Ship {
+  constructor(length) {
+    this.length = length
+    this.health = length
+  }
+
+  damage() {
+    if (this.health == 0) throw new Error("This ship is already destoryed")
+    this.health--
+  }
+}
+
+class Carrier extends Ship {
+  constructor() {
+    super(5)
+  }
+}
+
+class Battleship extends Ship {
+  constructor() {
+    super(4)
+  }
+}
+
+class Destroyer extends Ship {
+  constructor() {
+    super(3)
+  }
+}
+
+class Submarine extends Ship {
+  constructor() {
+    super(2)
+  }
+}
+
+export { Carrier, Battleship, Destroyer, Submarine }
