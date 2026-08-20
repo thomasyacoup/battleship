912d5c6587f98b4d5795f5caf2c64045798109c0
thomas011001 <thomasyacoub321@gmail.com>
2026-08-20 05:39:29 +0300

    FEAT: update package.json for deployment and add homepage; configure webpack public path

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: "/battleship/"
   },
   devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval-source-map',
   devServer: {
